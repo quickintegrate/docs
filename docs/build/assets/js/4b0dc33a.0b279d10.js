b45e071b004e15d50a288c0fdb2f544e9861b04e
pranabroygithub <pranabroyofficial@gmail.com>
2026-01-30 06:09:41 +0530

    updated getting started->postal rest API service docs

---
+++ docs/build/assets/js/4b0dc33a.0b279d10.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuser_documentation=self.webpackChunkuser_documentation||[]).push([[7720],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ docs/build/assets/js/5e95c892.0ace0eca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuser_documentation=self.webpackChunkuser_documentation||[]).push([[9661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(7294);var u=r(6010),n=r(1944),t=r(5281),a=r(8790),c=r(7961),o=r(5893);function d(e){return(0,o.jsx)(n.FG,{className:(0,u.Z)(t.k.wrapper.docsPages),children:(0,o.jsx)(c.Z,{children:(0,a.H)(e.route.routes)})})}}}]);
